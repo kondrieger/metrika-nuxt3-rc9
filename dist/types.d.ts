6a3ee4ea33026ad943f97702d27d908861155980
Nikita Semenov <kondrieger@gmail.com>
2022-12-07 16:58:42 +0300

    urls changed

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,6 +1,11 @@
 
-import {  } from './module'
+import { ModuleOptions, ModuleHooks } from './module'
 
+declare module '@nuxt/schema' {
+  interface NuxtConfig { ['yandexMetrika']?: Partial<ModuleOptions> }
+  interface NuxtOptions { ['yandexMetrika']?: ModuleOptions }
+  interface NuxtHooks extends ModuleHooks {}
+}
 
 
 export { default } from './module'
